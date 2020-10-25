51bb20c157de0bbe106f414f2efcdb1ff5ae14e3
Mostafa <mustafa.husseiny@gmail.com>
2020-10-25 20:22:45 +0100

    Updates

---
+++ precache-manifest.402ed159f9e08ac7b1c41ba9b4a6956e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f51c9c7c44efe64bf4ca300c561acd",
+    "url": "/http//freelancer2020/github.io/nfx/index.html"
+  },
+  {
+    "revision": "3ce34a617f804e8fac16",
+    "url": "/http//freelancer2020/github.io/nfx/static/css/main.ae2e05a5.chunk.css"
+  },
+  {
+    "revision": "f7f72703d76ea96365ff",
+    "url": "/http//freelancer2020/github.io/nfx/static/js/2.91e672b0.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/http//freelancer2020/github.io/nfx/static/js/2.91e672b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce34a617f804e8fac16",
+    "url": "/http//freelancer2020/github.io/nfx/static/js/main.7c263224.chunk.js"
+  },
+  {
+    "revision": "7125621fae60709c5962",
+    "url": "/http//freelancer2020/github.io/nfx/static/js/runtime-main.37597be1.js"
+  }
+]);
